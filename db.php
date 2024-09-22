5af9bcdadc48c0763aab53e6e0a513f761469961
NeXoS355 <59335764+NeXoS355@users.noreply.github.com>
2024-09-22 22:08:25 +0200

    Add files via upload

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
 $host = 'localhost';
 $dbname = 'bookApp';
-$username = '<yourUser>';
-$password = '<yourPassword>';
+$username = '<YourUser>';
+$password = '<YourPassword>';
 
 try {
 //    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
